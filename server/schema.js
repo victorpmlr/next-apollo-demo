8927b5b0d71aa577af6b78b7d481df0d8eccc5b8
Victor Pamelard <victor.pamelard@gmail.com>
2021-08-21 16:50:45 +0200

    people graphql endpoint

--- server/schema.js
+++ server/schema.js
@@ -1,10 +1,30 @@
 const {
-  graphql,
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
 } = require('graphql')
 const faker = require('faker')
+
+const PersonType = new GraphQLObjectType({
+  name: 'Person',
+  fields: () => ({
+    name: { type: GraphQLString },
+    address: {
+      type: new GraphQLObjectType({
+        name: 'Address',
+        fields: () => ({
+          streetAddress: { type: GraphQLString },
+          city: { type: GraphQLString },
+          country: { type: GraphQLString },
+        })
+      })
+    },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  })
+})
 
 module.exports = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -14,6 +34,27 @@ module.exports = new GraphQLSchema({
         type: GraphQLString,
         resolve() {
           return faker.name.findName()
+        }
+      },
+      people: {
+        type: new GraphQLList(PersonType),
+        args: {
+          first: {
+            type: GraphQLInt,
+          },
+        },
+        resolve(_, args) {
+          const first = args.first || 2000
+          return Array.from(Array(first)).map(() => ({
+            name: faker.name.findName(),
+            address: {
+              streetAddress: faker.address.streetAddress,
+              city: faker.address.city,
+              country: faker.address.country,
+            },
+            email: faker.internet.email(),
+            phone: faker.phone.phoneNumber(),
+          }))
         }
       }
     }
