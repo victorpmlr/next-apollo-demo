853a8da605955aff828be0a9df9a2790df6648ef
Victor Pamelard <victor.pamelard@gmail.com>
2021-08-22 08:29:24 +0200

    limit 'first' arg to 100

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,13 @@
 const faker = require('faker')
 const data = require('./data.json')
+const { UserInputError } = require('apollo-server-express')
 
 const resolvers = {
   Query: {
     name: () => faker.name.findName(),
     people: (_, args) => {
       const { first, after, name } = args
+      if (first > 100) throw new UserInputError('First is limited to 100.')
       if (name && name.length < 3) return []
       const people = name
         ? data.people.filter(
